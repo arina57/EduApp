9584d3a8115611650f468a461b97f933f6f011f1
Arina Anjong <arina_57@hotmail.co.jp>
2020-07-09 14:06:48 +0900

    nuget update

---
+++ EduApp/EduApp.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using CrossLibrary;
+
+namespace EduApp.Core.ViewModels {
+    public class MainViewModel : CrossViewModel {
+        public MainViewModel() {
+        }
+    }
+}
---
+++ EduApp/EduApp.Droid/Views/MainView.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using CrossLibrary.Droid.Views;
+using EduApp.Core.ViewModels;
+
+namespace EduApp.Droid.Views {
+    public class MainView : CrossFragment<MainViewModel> {
+        public override void OnCreate(Bundle savedInstanceState) {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            // Use this to return your custom view for this Fragment
+            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+
+            return base.OnCreateView(inflater, container, savedInstanceState);
+        }
+    }
+}
