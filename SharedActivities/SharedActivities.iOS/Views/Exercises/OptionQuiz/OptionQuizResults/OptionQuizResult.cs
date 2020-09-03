0a8aa139083d897a8618edf0c0f85181c2934b43
Arina Anjong <arina_57@hotmail.co.jp>
2020-09-03 15:21:15 +0900

    added results view for option quiz

---
+++ SharedActivities/SharedActivities.iOS/Views/Exercises/OptionQuiz/OptionQuizResults/OptionQuizResult.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using CrossLibrary.iOS.Views;
+using Foundation;
+using SharedActivities.Core.ViewModels.Exercises.Results;
+using UIKit;
+
+namespace SharedActivities.iOS.Views.Exercises.OptionQuiz.OptionQuizResults {
+    public partial class OptionQuizResult : CrossUIViewController<OptionQuizResultViewModel> {
+
+
+        public OptionQuizResult() {
+        }
+
+        public override void RefreshUILocale() {
+            TitleLabel.Text = string.Empty; //Resx.String.Results;
+            ResultsTable.ReloadData();
+        }
+
+        public override void ViewDidLoad() {
+            base.ViewDidLoad();
+            ResultsTable.RegisterNibForCellReuse(OptionQuizResultCell.Nib, "ResultsReuseCell");
+            ResultsTable.Source = new ResultsSource(this);
+        }
+
+        public override void ViewWillAppear(bool animated) {
+            base.ViewWillAppear(animated);
+            ScrollView.ScrollRectToVisible(new CoreGraphics.CGRect(0, 0, 1, 1), false);
+            ResultsTable.ScrollRectToVisible(new CoreGraphics.CGRect(0, 0, 1, 1), false);
+        }
+
+        private class ResultsSource : UITableViewSource {
+            private OptionQuizResult resultExplaination;
+            private OptionQuizResultViewModel ViewModel => resultExplaination.ViewModel;
+            public ResultsSource(OptionQuizResult resultExplaination) {
+                this.resultExplaination = resultExplaination;
+            }
+
+            public override void RowSelected(UITableView tableView, NSIndexPath indexPath) {
+                tableView.DeselectRow(indexPath, false);
+                var cell = tableView.CellAt(indexPath) as OptionQuizResultCell;
+                cell.Expanded = !cell.Expanded;
+                tableView.ReloadData();
+                tableView.LayoutSubviews();
+                cell.LayoutSubviews();
+
+                //tableView.ReloadRows(new NSIndexPath[] { indexPath }, UITableViewRowAnimation.Automatic);
+            }
+
+            public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath) {
+                var cell = tableView.DequeueReusableCell("ResultsReuseCell") as OptionQuizResultCell;
+                if (cell.QuestionNumber == -1) {
+
+                }
+
+                cell.Setup(ViewModel, indexPath.Row);
+                //cell.Expanded = false;
+                return cell;
+            }
+
+            public override nint RowsInSection(UITableView tableview, nint section) => ViewModel.OptionQuiz.TotalNumberOfQuestions;
+        }
+    }
+}
---
+++ SharedActivities/SharedActivities.iOS/Views/Exercises/OptionQuiz/OptionQuizResults/OptionQuizResult.designer.cs
@@ -0,0 +1,54 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using SharedActivities.iOS.CustomViews;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace SharedActivities.iOS.Views.Exercises.OptionQuiz.OptionQuizResults {
+    [Register("OptionQuizResult")]
+    partial class OptionQuizResult {
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        ContentSizedTableView ResultsTable { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        CrossLibrary.iOS.Views.CrossContainerView ScoreViewContainer { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UIScrollView ScrollView { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UILabel TitleLabel { get; set; }
+
+        void ReleaseDesignerOutlets() {
+            if (ResultsTable != null) {
+                ResultsTable.Dispose();
+                ResultsTable = null;
+            }
+
+            if (ScoreViewContainer != null) {
+                ScoreViewContainer.Dispose();
+                ScoreViewContainer = null;
+            }
+
+            if (ScrollView != null) {
+                ScrollView.Dispose();
+                ScrollView = null;
+            }
+
+            if (TitleLabel != null) {
+                TitleLabel.Dispose();
+                TitleLabel = null;
+            }
+        }
+    }
+}
