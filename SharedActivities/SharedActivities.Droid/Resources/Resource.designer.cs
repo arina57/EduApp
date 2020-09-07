b45e6e09a3ed87e3cb342dbc8df8372bf9f29210
Arina Anjong <arina_57@hotmail.co.jp>
2020-09-07 16:23:14 +0900

    added dialogue option quiz view

--- SharedActivities/SharedActivities.Droid/Resources/Resource.designer.cs
+++ SharedActivities/SharedActivities.Droid/Resources/Resource.designer.cs
@@ -111,28 +111,34 @@ namespace SharedActivities.Droid
 			public static int ScaleFadeIn = 2130771995;
 			
 			// aapt resource value: 0x7F01001C
-			public static int slide_in_from_bottom = 2130771996;
+			public static int ScaleFadeInAndPulse = 2130771996;
 			
 			// aapt resource value: 0x7F01001D
-			public static int slide_in_from_left = 2130771997;
+			public static int ScaleFadeOut = 2130771997;
 			
 			// aapt resource value: 0x7F01001E
-			public static int slide_in_from_right = 2130771998;
+			public static int slide_in_from_bottom = 2130771998;
 			
 			// aapt resource value: 0x7F01001F
-			public static int slide_in_from_top = 2130771999;
+			public static int slide_in_from_left = 2130771999;
 			
 			// aapt resource value: 0x7F010020
-			public static int slide_out_bottom = 2130772000;
-			
-			// aapt resource value: 0x7F010023
-			public static int slide_out_top = 2130772003;
+			public static int slide_in_from_right = 2130772000;
 			
 			// aapt resource value: 0x7F010021
-			public static int slide_out_to_left = 2130772001;
+			public static int slide_in_from_top = 2130772001;
 			
 			// aapt resource value: 0x7F010022
-			public static int slide_out_to_right = 2130772002;
+			public static int slide_out_bottom = 2130772002;
+			
+			// aapt resource value: 0x7F010025
+			public static int slide_out_top = 2130772005;
+			
+			// aapt resource value: 0x7F010023
+			public static int slide_out_to_left = 2130772003;
+			
+			// aapt resource value: 0x7F010024
+			public static int slide_out_to_right = 2130772004;
 			
 			static Animation()
 			{
@@ -2815,259 +2821,271 @@ namespace SharedActivities.Droid
 			public static int progressBar = 2131230880;
 			
 			// aapt resource value: 0x7F0800A1
-			public static int progress_circular = 2131230881;
+			public static int progressView = 2131230881;
 			
 			// aapt resource value: 0x7F0800A2
-			public static int progress_horizontal = 2131230882;
+			public static int progress_circular = 2131230882;
 			
 			// aapt resource value: 0x7F0800A3
-			public static int questionIconLottieView = 2131230883;
+			public static int progress_horizontal = 2131230883;
 			
 			// aapt resource value: 0x7F0800A4
-			public static int questionTextView = 2131230884;
-			
-			// aapt resource value: 0x7F0800A5
-			public static int radio = 2131230885;
+			public static int questionIconLottieView = 2131230884;
 			
 			// aapt resource value: 0x7F0800A6
-			public static int restart = 2131230886;
+			public static int questionsView = 2131230886;
+			
+			// aapt resource value: 0x7F0800A5
+			public static int questionTextView = 2131230885;
 			
 			// aapt resource value: 0x7F0800A7
-			public static int resultImageView = 2131230887;
+			public static int radio = 2131230887;
 			
 			// aapt resource value: 0x7F0800A8
-			public static int resultsList = 2131230888;
+			public static int restart = 2131230888;
 			
 			// aapt resource value: 0x7F0800A9
-			public static int resultsTitleTextView = 2131230889;
+			public static int resultImageView = 2131230889;
 			
 			// aapt resource value: 0x7F0800AA
-			public static int reverse = 2131230890;
+			public static int resultsList = 2131230890;
 			
 			// aapt resource value: 0x7F0800AB
-			public static int right = 2131230891;
+			public static int resultsTitleTextView = 2131230891;
 			
 			// aapt resource value: 0x7F0800AC
-			public static int right_icon = 2131230892;
+			public static int reverse = 2131230892;
 			
 			// aapt resource value: 0x7F0800AD
-			public static int right_side = 2131230893;
+			public static int right = 2131230893;
 			
 			// aapt resource value: 0x7F0800AE
-			public static int roleTextView = 2131230894;
+			public static int right_icon = 2131230894;
 			
 			// aapt resource value: 0x7F0800AF
-			public static int scoreTextView = 2131230895;
+			public static int right_side = 2131230895;
 			
 			// aapt resource value: 0x7F0800B0
-			public static int screen = 2131230896;
+			public static int roleImageView = 2131230896;
 			
 			// aapt resource value: 0x7F0800B1
-			public static int scrollIndicatorDown = 2131230897;
+			public static int roleRecyclerView = 2131230897;
 			
 			// aapt resource value: 0x7F0800B2
-			public static int scrollIndicatorUp = 2131230898;
+			public static int roleTextView = 2131230898;
 			
 			// aapt resource value: 0x7F0800B3
-			public static int scrollView = 2131230899;
+			public static int scoreTextView = 2131230899;
 			
 			// aapt resource value: 0x7F0800B4
-			public static int search_badge = 2131230900;
+			public static int screen = 2131230900;
 			
 			// aapt resource value: 0x7F0800B5
-			public static int search_bar = 2131230901;
+			public static int scrollIndicatorDown = 2131230901;
 			
 			// aapt resource value: 0x7F0800B6
-			public static int search_button = 2131230902;
+			public static int scrollIndicatorUp = 2131230902;
 			
 			// aapt resource value: 0x7F0800B7
-			public static int search_close_btn = 2131230903;
+			public static int scrollView = 2131230903;
 			
 			// aapt resource value: 0x7F0800B8
-			public static int search_edit_frame = 2131230904;
+			public static int search_badge = 2131230904;
 			
 			// aapt resource value: 0x7F0800B9
-			public static int search_go_btn = 2131230905;
+			public static int search_bar = 2131230905;
 			
 			// aapt resource value: 0x7F0800BA
-			public static int search_mag_icon = 2131230906;
+			public static int search_button = 2131230906;
 			
 			// aapt resource value: 0x7F0800BB
-			public static int search_plate = 2131230907;
+			public static int search_close_btn = 2131230907;
 			
 			// aapt resource value: 0x7F0800BC
-			public static int search_src_text = 2131230908;
+			public static int search_edit_frame = 2131230908;
 			
 			// aapt resource value: 0x7F0800BD
-			public static int search_voice_btn = 2131230909;
+			public static int search_go_btn = 2131230909;
 			
 			// aapt resource value: 0x7F0800BE
-			public static int select_dialog_listview = 2131230910;
+			public static int search_mag_icon = 2131230910;
+			
+			// aapt resource value: 0x7F0800BF
+			public static int search_plate = 2131230911;
+			
+			// aapt resource value: 0x7F0800C0
+			public static int search_src_text = 2131230912;
+			
+			// aapt resource value: 0x7F0800C1
+			public static int search_voice_btn = 2131230913;
+			
+			// aapt resource value: 0x7F0800C2
+			public static int select_dialog_listview = 2131230914;
 			
 			// aapt resource value: 0x7F080004
 			public static int SHIFT = 2131230724;
 			
-			// aapt resource value: 0x7F0800BF
-			public static int shortcut = 2131230911;
-			
-			// aapt resource value: 0x7F0800C0
-			public static int showCustom = 2131230912;
-			
-			// aapt resource value: 0x7F0800C1
-			public static int showHome = 2131230913;
-			
-			// aapt resource value: 0x7F0800C2
-			public static int showTitle = 2131230914;
-			
 			// aapt resource value: 0x7F0800C3
-			public static int situationTextView = 2131230915;
+			public static int shortcut = 2131230915;
 			
 			// aapt resource value: 0x7F0800C4
-			public static int software = 2131230916;
+			public static int showCustom = 2131230916;
 			
 			// aapt resource value: 0x7F0800C5
-			public static int spacer = 2131230917;
+			public static int showHome = 2131230917;
 			
 			// aapt resource value: 0x7F0800C6
-			public static int split_action_bar = 2131230918;
+			public static int showTitle = 2131230918;
 			
 			// aapt resource value: 0x7F0800C7
-			public static int spread = 2131230919;
+			public static int situationTextView = 2131230919;
 			
 			// aapt resource value: 0x7F0800C8
-			public static int spread_inside = 2131230920;
+			public static int software = 2131230920;
 			
 			// aapt resource value: 0x7F0800C9
-			public static int src_atop = 2131230921;
+			public static int spacer = 2131230921;
 			
 			// aapt resource value: 0x7F0800CA
-			public static int src_in = 2131230922;
+			public static int split_action_bar = 2131230922;
 			
 			// aapt resource value: 0x7F0800CB
-			public static int src_over = 2131230923;
+			public static int spread = 2131230923;
 			
 			// aapt resource value: 0x7F0800CC
-			public static int standard = 2131230924;
+			public static int spread_inside = 2131230924;
 			
 			// aapt resource value: 0x7F0800CD
-			public static int start = 2131230925;
-			
-			// aapt resource value: 0x7F0800CF
-			public static int submenuarrow = 2131230927;
-			
-			// aapt resource value: 0x7F0800D0
-			public static int submit_area = 2131230928;
-			
-			// aapt resource value: 0x7F0800D1
-			public static int subtitleTextView = 2131230929;
+			public static int src_atop = 2131230925;
 			
 			// aapt resource value: 0x7F0800CE
-			public static int subTitleTextView = 2131230926;
+			public static int src_in = 2131230926;
+			
+			// aapt resource value: 0x7F0800CF
+			public static int src_over = 2131230927;
+			
+			// aapt resource value: 0x7F0800D0
+			public static int standard = 2131230928;
+			
+			// aapt resource value: 0x7F0800D1
+			public static int start = 2131230929;
+			
+			// aapt resource value: 0x7F0800D3
+			public static int submenuarrow = 2131230931;
+			
+			// aapt resource value: 0x7F0800D4
+			public static int submit_area = 2131230932;
+			
+			// aapt resource value: 0x7F0800D5
+			public static int subtitleTextView = 2131230933;
+			
+			// aapt resource value: 0x7F0800D2
+			public static int subTitleTextView = 2131230930;
 			
 			// aapt resource value: 0x7F080005
 			public static int SYM = 2131230725;
 			
-			// aapt resource value: 0x7F0800D2
-			public static int tabMode = 2131230930;
-			
-			// aapt resource value: 0x7F0800D3
-			public static int tag_accessibility_actions = 2131230931;
-			
-			// aapt resource value: 0x7F0800D4
-			public static int tag_accessibility_clickable_spans = 2131230932;
-			
-			// aapt resource value: 0x7F0800D5
-			public static int tag_accessibility_heading = 2131230933;
-			
 			// aapt resource value: 0x7F0800D6
-			public static int tag_accessibility_pane_title = 2131230934;
+			public static int tabMode = 2131230934;
 			
 			// aapt resource value: 0x7F0800D7
-			public static int tag_screen_reader_focusable = 2131230935;
+			public static int tag_accessibility_actions = 2131230935;
 			
 			// aapt resource value: 0x7F0800D8
-			public static int tag_transition_group = 2131230936;
+			public static int tag_accessibility_clickable_spans = 2131230936;
 			
 			// aapt resource value: 0x7F0800D9
-			public static int tag_unhandled_key_event_manager = 2131230937;
+			public static int tag_accessibility_heading = 2131230937;
 			
 			// aapt resource value: 0x7F0800DA
-			public static int tag_unhandled_key_listeners = 2131230938;
+			public static int tag_accessibility_pane_title = 2131230938;
 			
 			// aapt resource value: 0x7F0800DB
-			public static int text = 2131230939;
+			public static int tag_screen_reader_focusable = 2131230939;
 			
 			// aapt resource value: 0x7F0800DC
-			public static int text2 = 2131230940;
+			public static int tag_transition_group = 2131230940;
 			
 			// aapt resource value: 0x7F0800DD
-			public static int textSpacerNoButtons = 2131230941;
+			public static int tag_unhandled_key_event_manager = 2131230941;
 			
 			// aapt resource value: 0x7F0800DE
-			public static int textSpacerNoTitle = 2131230942;
+			public static int tag_unhandled_key_listeners = 2131230942;
 			
 			// aapt resource value: 0x7F0800DF
-			public static int textView = 2131230943;
+			public static int text = 2131230943;
 			
 			// aapt resource value: 0x7F0800E0
-			public static int time = 2131230944;
+			public static int text2 = 2131230944;
 			
 			// aapt resource value: 0x7F0800E1
-			public static int timesCompletedTextView = 2131230945;
+			public static int textSpacerNoButtons = 2131230945;
 			
 			// aapt resource value: 0x7F0800E2
-			public static int timesPerfectTextView = 2131230946;
+			public static int textSpacerNoTitle = 2131230946;
 			
 			// aapt resource value: 0x7F0800E3
-			public static int title = 2131230947;
+			public static int textView = 2131230947;
 			
 			// aapt resource value: 0x7F0800E4
-			public static int titleDividerNoCustom = 2131230948;
+			public static int time = 2131230948;
 			
 			// aapt resource value: 0x7F0800E5
-			public static int titleTextView = 2131230949;
+			public static int timesCompletedTextView = 2131230949;
 			
 			// aapt resource value: 0x7F0800E6
-			public static int title_template = 2131230950;
+			public static int timesPerfectTextView = 2131230950;
 			
 			// aapt resource value: 0x7F0800E7
-			public static int top = 2131230951;
+			public static int title = 2131230951;
 			
 			// aapt resource value: 0x7F0800E8
-			public static int topFrame = 2131230952;
+			public static int titleDividerNoCustom = 2131230952;
 			
 			// aapt resource value: 0x7F0800E9
-			public static int topPanel = 2131230953;
+			public static int titleTextView = 2131230953;
 			
 			// aapt resource value: 0x7F0800EA
-			public static int @unchecked = 2131230954;
+			public static int title_template = 2131230954;
 			
 			// aapt resource value: 0x7F0800EB
-			public static int uniform = 2131230955;
+			public static int top = 2131230955;
 			
 			// aapt resource value: 0x7F0800EC
-			public static int up = 2131230956;
+			public static int topFrame = 2131230956;
 			
 			// aapt resource value: 0x7F0800ED
-			public static int useLogo = 2131230957;
+			public static int topPanel = 2131230957;
 			
 			// aapt resource value: 0x7F0800EE
-			public static int vocabRecyclerView = 2131230958;
+			public static int @unchecked = 2131230958;
 			
 			// aapt resource value: 0x7F0800EF
-			public static int vocabTextView = 2131230959;
+			public static int uniform = 2131230959;
 			
 			// aapt resource value: 0x7F0800F0
-			public static int waitingImageView = 2131230960;
+			public static int up = 2131230960;
 			
 			// aapt resource value: 0x7F0800F1
-			public static int withText = 2131230961;
+			public static int useLogo = 2131230961;
 			
 			// aapt resource value: 0x7F0800F2
-			public static int wrap = 2131230962;
+			public static int vocabRecyclerView = 2131230962;
 			
 			// aapt resource value: 0x7F0800F3
-			public static int wrap_content = 2131230963;
+			public static int vocabTextView = 2131230963;
+			
+			// aapt resource value: 0x7F0800F4
+			public static int waitingImageView = 2131230964;
+			
+			// aapt resource value: 0x7F0800F5
+			public static int withText = 2131230965;
+			
+			// aapt resource value: 0x7F0800F6
+			public static int wrap = 2131230966;
+			
+			// aapt resource value: 0x7F0800F7
+			public static int wrap_content = 2131230967;
 			
 			static Id()
 			{
@@ -3244,67 +3262,73 @@ namespace SharedActivities.Droid
 			public static int default_activity_layout = 2131427360;
 			
 			// aapt resource value: 0x7F0B0021
-			public static int gap_fill = 2131427361;
+			public static int dialogue_option_quiz = 2131427361;
 			
 			// aapt resource value: 0x7F0B0022
-			public static int gap_fill_cell = 2131427362;
+			public static int dialogue_option_quiz_role_cell = 2131427362;
 			
 			// aapt resource value: 0x7F0B0023
-			public static int gap_fill_vocab_cell = 2131427363;
+			public static int gap_fill = 2131427363;
 			
 			// aapt resource value: 0x7F0B0024
-			public static int notification_action = 2131427364;
+			public static int gap_fill_cell = 2131427364;
 			
 			// aapt resource value: 0x7F0B0025
-			public static int notification_action_tombstone = 2131427365;
+			public static int gap_fill_vocab_cell = 2131427365;
 			
 			// aapt resource value: 0x7F0B0026
-			public static int notification_template_custom_big = 2131427366;
+			public static int notification_action = 2131427366;
 			
 			// aapt resource value: 0x7F0B0027
-			public static int notification_template_icon_group = 2131427367;
+			public static int notification_action_tombstone = 2131427367;
 			
 			// aapt resource value: 0x7F0B0028
-			public static int notification_template_part_chronometer = 2131427368;
+			public static int notification_template_custom_big = 2131427368;
 			
 			// aapt resource value: 0x7F0B0029
-			public static int notification_template_part_time = 2131427369;
+			public static int notification_template_icon_group = 2131427369;
 			
 			// aapt resource value: 0x7F0B002A
-			public static int option_quiz = 2131427370;
+			public static int notification_template_part_chronometer = 2131427370;
 			
 			// aapt resource value: 0x7F0B002B
-			public static int option_quiz_answer_cell = 2131427371;
+			public static int notification_template_part_time = 2131427371;
 			
 			// aapt resource value: 0x7F0B002C
-			public static int option_quiz_one_in_four_cell = 2131427372;
+			public static int option_quiz = 2131427372;
 			
 			// aapt resource value: 0x7F0B002D
-			public static int practice_heading = 2131427373;
+			public static int option_quiz_answer_cell = 2131427373;
 			
 			// aapt resource value: 0x7F0B002E
-			public static int score_page_with_explanation = 2131427374;
+			public static int option_quiz_one_in_four_cell = 2131427374;
 			
 			// aapt resource value: 0x7F0B002F
-			public static int scoring = 2131427375;
+			public static int practice_heading = 2131427375;
 			
 			// aapt resource value: 0x7F0B0030
-			public static int select_dialog_item_material = 2131427376;
+			public static int score_page_with_explanation = 2131427376;
 			
 			// aapt resource value: 0x7F0B0031
-			public static int select_dialog_multichoice_material = 2131427377;
+			public static int scoring = 2131427377;
 			
 			// aapt resource value: 0x7F0B0032
-			public static int select_dialog_singlechoice_material = 2131427378;
+			public static int select_dialog_item_material = 2131427378;
 			
 			// aapt resource value: 0x7F0B0033
-			public static int support_simple_spinner_dropdown_item = 2131427379;
+			public static int select_dialog_multichoice_material = 2131427379;
 			
 			// aapt resource value: 0x7F0B0034
-			public static int unit_practice = 2131427380;
+			public static int select_dialog_singlechoice_material = 2131427380;
 			
 			// aapt resource value: 0x7F0B0035
-			public static int unit_practice_activity_select_cell = 2131427381;
+			public static int support_simple_spinner_dropdown_item = 2131427381;
+			
+			// aapt resource value: 0x7F0B0036
+			public static int unit_practice = 2131427382;
+			
+			// aapt resource value: 0x7F0B0037
+			public static int unit_practice_activity_select_cell = 2131427383;
 			
 			static Layout()
 			{
---
+++ SharedActivities/SharedActivities.Droid/Views/Exercises/DialogueOptionQuiz.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Android.Animation;
+using Android.OS;
+using Android.Views;
+using Android.Views.Animations;
+using Android.Widget;
+using AndroidX.RecyclerView.Widget;
+using Com.Airbnb.Lottie;
+using CrossLibrary.Droid.Views;
+using SharedActivities.Core.ViewModels.Exercises;
+using SharedActivities.Droid.CustomViews;
+
+namespace SharedActivities.Droid.Views.Exercises {
+    public class DialogueOptionQuiz : CrossFragment<DialogueOptionQuizViewModel> {
+        RecyclerView roleRecyclerView;
+        private DiscreteProgressView progressView;
+        private RoleListAdapter roleListAdapter;
+        public DialogueOptionQuiz() {
+
+        }
+
+        public override void Prepare(DialogueOptionQuizViewModel model) {
+            base.Prepare(model);
+            ViewModel.ScoringViewModel.ScoreChanged += this.Logic_ScoreChanged;
+            //ViewModel.ExerciseFinished += this.Logic_ExerciseFinished;
+        }
+        //bool AutoPopupScoreWhenDone { get; set; } = false;
+
+
+
+        public override void OnCreate(Bundle savedInstanceState) {
+            base.OnCreate(savedInstanceState);
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            // Use this to return your custom view for this Fragment
+            var view = inflater.Inflate(Resource.Layout.dialogue_option_quiz, container, false);
+            roleRecyclerView = view.FindViewById<RecyclerView>(Resource.Id.roleRecyclerView);
+            progressView = view.FindViewById<DiscreteProgressView>(Resource.Id.progressView);
+
+            progressView.ProgressTracker = ViewModel;
+            roleRecyclerView.SetLayoutManager(new LinearLayoutManager(this.Activity, LinearLayoutManager.Horizontal, false));
+            roleListAdapter = new RoleListAdapter(this);
+            roleRecyclerView.SetAdapter(roleListAdapter);
+            roleRecyclerView.OverScrollMode = OverScrollMode.Never;
+            return view;
+        }
+
+        //private void Logic_ExerciseFinished(object sender, EventArgs e) {
+        //    if(AutoPopupScoreWhenDone) {
+        //        var chooseBestSentenceResults = new ResultExplanation(ViewModel.OptionQuizViewModel);
+        //        chooseBestSentenceResults.ShowOver();
+        //    }
+        //}
+
+        private void Logic_ScoreChanged(object sender, EventArgs e) {
+            RefreshUILocale();
+            roleListAdapter.NotifyDataSetChanged();
+            if (roleListAdapter.ItemCount > 0) {
+                roleRecyclerView.SmoothScrollToPosition(ViewModel.CurrentRoleId - 1);
+            }
+
+        }
+
+        public override void OnPause() {
+            base.OnPause();
+        }
+
+        public override void RefreshUILocale() {
+            roleRecyclerView.Visibility = ViewModel.Finished ? ViewStates.Invisible : ViewStates.Visible;
+        }
+
+
+
+        private class RoleListAdapter : RecyclerView.Adapter {
+            private DialogueOptionQuiz chooseBestSentence;
+            DialogueOptionQuizViewModel chooseBestSentenceLogic;
+            public RoleListAdapter(DialogueOptionQuiz chooseBestSentence) {
+                this.chooseBestSentence = chooseBestSentence;
+                chooseBestSentenceLogic = chooseBestSentence.ViewModel;
+            }
+
+            public override int ItemCount => chooseBestSentenceLogic.RoleCount;
+
+            public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+                var viewHolder = (ViewHolder)holder;
+                viewHolder.RoleImageView.SetAnimationFromJson(chooseBestSentenceLogic.GetRoleImageJson(position), chooseBestSentenceLogic.GetRoleImageJson(position));
+                //viewHolder.RoleImageView.SetColor(position == 0 ? Color.Rgb(0, 0, 50) : Color.Rgb(50, 0, 0));
+                viewHolder.View.LayoutParameters.Width = (int)(chooseBestSentence.roleRecyclerView.Width / (float)ItemCount);
+                viewHolder.RoleImageView.Progress = 0;
+                viewHolder.RoleImageView.RepeatCount = ValueAnimator.Infinite;
+                viewHolder.RoleImageView.RepeatMode = 1;
+                viewHolder.RoleImageView.PlayAnimation();
+                viewHolder.RoleTextView.Text = chooseBestSentenceLogic.GetRoleName(position);
+                if (chooseBestSentenceLogic.IsCurrentRole(position)) {
+                    viewHolder.Select();
+                } else {
+                    viewHolder.Deselect();
+                }
+            }
+
+            public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType) {
+                var view = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.dialogue_option_quiz_role_cell, parent, false);
+                view.LayoutParameters.Width = (int)(parent.Width / (float)ItemCount);
+                var viewHolder = new ViewHolder(view);
+                return viewHolder;
+            }
+
+            private class ViewHolder : RecyclerView.ViewHolder {
+                public View View { get; private set; }
+                public LottieAnimationView RoleImageView { get; private set; }
+                public TextView RoleTextView { get; private set; }
+
+
+                private Animation outSet;
+                private Animation inSet;
+
+                public ViewHolder(View view) : base(view) {
+                    this.View = view;
+                    RoleImageView = view.FindViewById<LottieAnimationView>(Resource.Id.roleImageView);
+                    RoleTextView = view.FindViewById<TextView>(Resource.Id.roleTextView);
+                    inSet = AnimationUtils.LoadAnimation(View.Context, Resource.Animation.ScaleFadeInAndPulse); // new AnimationSet(true) { FillEnabled = true, FillAfter = true };
+                    outSet = AnimationUtils.LoadAnimation(View.Context, Resource.Animation.ScaleFadeOut);
+                }
+
+                public void Select() {
+                    RoleImageView.StartAnimation(inSet);
+                }
+
+                public void Deselect() {
+                    RoleImageView.StartAnimation(outSet);
+                }
+
+            }
+        }
+    }
+}
