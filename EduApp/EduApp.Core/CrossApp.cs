737855e51e31fbff393866fc13483d8167d1de1e
Arina Anjong <arina_57@hotmail.co.jp>
2020-09-15 13:47:47 +0900

    changed color palette

--- EduApp/EduApp.Core/CrossApp.cs
+++ EduApp/EduApp.Core/CrossApp.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using EduApp.Core.ViewModels;
 
 namespace EduApp.Core {
@@ -8,6 +9,13 @@ namespace EduApp.Core {
 
         public override void AppLoaded() {
             base.AppLoaded();
+            SharedActivities.Core.GlobalColorPalette.VeryDark = Xamarin.Essentials.ColorConverters.FromHex("#253237");
+            SharedActivities.Core.GlobalColorPalette.Dark = Xamarin.Essentials.ColorConverters.FromHex("#5c6b73");
+            SharedActivities.Core.GlobalColorPalette.Medium = Xamarin.Essentials.ColorConverters.FromHex("#9db4c0");
+            SharedActivities.Core.GlobalColorPalette.Light = Xamarin.Essentials.ColorConverters.FromHex("#c2dfe3");
+            SharedActivities.Core.GlobalColorPalette.VeryLight = Xamarin.Essentials.ColorConverters.FromHex("#e0fbfc");
+
+
             var main = new MainViewModel();
             main.Show(false);
         }
